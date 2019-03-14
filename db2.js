0ac8f85014c6f8a27568935080ae186f3bd0dfed
kevinjang <kevinjang@163.com>
2019-03-14 17:32:11 +0800

    20190314
    1.实现分页查询

--- db2.js
+++ db2.js
@@ -7,29 +7,79 @@ const config = {
     database: 'cmstest'
 }
 // sql.connect()
-query = async () => {
+
+query = async ({parentid, pageSize,currentPage}) => {
     let result;
+    let tcount = pageSize || 10
     result = await (async () => {
         try {
             let pool = await sql.connect(config)
+            let start = (currentPage - 1) * tcount
+
+
+            console.log('start',start)
+
             let result1 = await pool.request()
-                .query('select top 10 * from dbo.Brc_OC_OUBaseInfo where RecordStatus = 1');
+                // .input()
+                .query(`select top ${tcount} * from
+                (SELECT ROW_NUMBER() over(order by ordnum) as rownum,
+                        [Id]
+                        ,[Name]
+                        ,[OULevel]
+                        ,[OrdNum]
+                        ,[RecordCreationTime]
+                        ,[LastUpdateTime]
+                        ,case [RecordStatus] when '2' then '已删除' 
+                            when '1' then '在用' end [RecordStatus]
+                  FROM [CMSTEST].[dbo].[Brc_OC_OUBaseInfo]
+                  where ParentID = ${parentid}
+                  and RecordStatus = 1) x
+                  where x.rownum > ${start}
+                `);
 
             // const rs = result1.recordsets;
 
+            let allcount;// parentid下面共计行数
+
+            allcount = await pool.request()
+            .query(`select count(1) allcount from dbo.Brc_OC_OUBaseInfo where RecordStatus = 1 and ParentID = ${parentid}`);
+
             pool.close();
             sql.close();
-            return result1
+            return {result1,allcount}
         } catch (error) {
             throw error
         }
     })()
 
     sql.on('error', err => {
-        console.log(err)
+        // console.log(err)
     });
 
     return result;
 }
 
-module.exports = { query }
+getColumnsRelation = async (tableName)=>{
+    let result ;
+    result = await (async ()=>{
+        try {
+            let pool = await sql.connect(config)
+            let result1 = await pool.request()
+            .query(`select * from dbo.TableColumnTitleRelations where TableName = '${tableName}'`);
+
+            pool.close();
+            sql.close();
+            return result1;
+        } catch (error) {
+            throw error
+        }
+    })()
+
+    sql.on('error',err=>{
+        throw err
+    });
+
+    return result;
+}
+
+module.exports = { query, getColumnsRelation }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { query } = require('./db2')
+const { query, getColumnsRelation } = require('./db2')
 const app = express()
 
 // app.use
@@ -14,41 +14,50 @@ app.all('*', function (req, res, next) {
 })
 
 app.get('/api/getOUBaseInfoAll', (req, res, next) => {
-    query().then(result => {
-        const rows = result.recordsets[0];
+
+    const { parentid,pageSize,currentPage } = req.query
+    // console.log('params',params)
+    query({parentid,count:pageSize,currentPage}).then(resultx => {
+        const {result1,allcount} = resultx
+        // console.log('result1',result1)
+        // console.log('allcount',allcount.recordset[0].allcount)
+        const allcountReal = allcount.recordset[0].allcount
+        const rows = result1.recordsets[0].sort((v1,v2)=>{
+            return v1.OrdNum > v2.OrdNum
+        });
 
         let colInfo = [];
 
-        if(rows && rows.length > 0){
+        if (rows && rows.length > 0) {
             const item = rows[0]
-            for (var item1 in item) {
-                const col = {
-                    dataIndex: item1,
-                    key: item1,
-                    title: item1
+
+            let tcr = [];
+            getColumnsRelation('OUBaseInfo').then(res1 => {
+                tcr = res1.recordsets[0]
+
+                for (var item1 in item) {
+                    for (let colx in tcr) {
+                        const tcrItem = tcr[colx]
+                        if (tcrItem.ColumnName === item1) {
+                            const col = {
+                                dataIndex: item1,
+                                key: item1,
+                                title: tcrItem.ColumnTitle
+                            }
+                            colInfo.push(col)
+                        }
+                    }
                 }
-                
-                colInfo.push(col)
-            }
+                res.json({
+                    datasource: rows,
+                    columns: colInfo,
+                    allcount: allcountReal
+                })
+            }).catch(err => {
+                console.log('err', err)
+            })
         }
-
-        // rows.forEach(item => {
-        //     for (var item1 in item) {
-        //         const col = {
-        //             dataIndex: item1,
-        //             key: item1
-        //         }
-
-        //         colInfo.push(col)
-        //     }
-        // })
-
-        res.json({
-            datasource: rows,
-            columns: colInfo
-        })
-    })
-        .catch(err => {
+    }).catch(err => {
             console.log('err', err)
         });
 })
